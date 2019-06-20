96cdac003900b82db6534271c18ad2af4b08cc6a
MuskChan <523919565@qq.com>
2019-06-20 10:35:37 +0800

    使用局部视图重构、数据填充、权限系统

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,7 +7,15 @@ use App\Http\Requests;
 use Auth;
 
 class SessionsController extends Controller
-{
+{  
+    //只让未登录用户访问登录页面：
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
     public function create()
     {
         return view('sessions.create');
@@ -22,7 +30,7 @@ class SessionsController extends Controller
 
        if (Auth::attempt($credentials, $request->has('remember'))) {
            session()->flash('success', '欢迎回来！');
-           return redirect()->route('users.show', [Auth::user()]);
+           return redirect()->intended(route('users.show', [Auth::user()]));
        } else {
            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
            return redirect()->back();
@@ -30,7 +38,7 @@ class SessionsController extends Controller
 
        return;
     }
-    
+
     public function destroy()
     {
         Auth::logout();
